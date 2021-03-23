f572843ca730b360bee10bc0bbbc93b217b1be9c
falakdesai <falak.desai@yahoo.com>
2021-03-23 00:46:35 -0400

    French TextSpeech -- just check out MainActivity.java file code.. nothing rocket science

---
+++ app/src/main/java/com/example/class6/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.class6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextToSpeech t1;
+    EditText ed1;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ed1= findViewById(R.id.editText);
+        b1= findViewById(R.id.button);
+
+        t1=new TextToSpeech(getApplicationContext(), status1 -> {
+            if(status1 != TextToSpeech.ERROR) {
+                t1.setLanguage(Locale.CANADA_FRENCH);
+            }
+        });
+
+        b1.setOnClickListener(task1 -> {
+            String toSpeak = ed1.getText().toString();
+            Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+            t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+        });
+    }
+
+    public void onPause(){
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
+
+}
